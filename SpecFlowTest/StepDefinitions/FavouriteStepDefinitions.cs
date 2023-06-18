fe8b510a7da53d860878751b45da82cd88457b10
sielth <74706401+sielth@users.noreply.github.com>
2023-06-18 16:23:21 +0200

    Update FavouriteStepDefinitions.cs

--- SpecFlowTest/StepDefinitions/FavouriteStepDefinitions.cs
+++ SpecFlowTest/StepDefinitions/FavouriteStepDefinitions.cs
@@ -3,52 +3,46 @@ using Microsoft.eShopWeb.ApplicationCore.Entities.FavouriteAggregate;
 namespace SpecFlowTest.StepDefinitions
 {
     [Binding]
-    public sealed class  FavouriteStepDefinitions
+    public sealed class FavouriteStepDefinitions
     {
         private int id;
-        private decimal _price;
         private readonly Favourite favourite = new Favourite("buyer1");
 
-        [Given("A Favourite Item (.*),(.*)")]
-        public void GivenTheFirstItemIs(int catalogItemId, decimal price)
+        [Given("an item with id (.*)")]
+        public void GivenAnItemWithId(int catalogItemId)
         {
             id = catalogItemId;
-            _price = price;
         }
 
-        [When("The item is added to FavouriteBasket")]
-        public void WhenTheItemIsAddedToFavouriteBasket()
+        [When("the item is added into the favourite list")]
+        public void WhenTheItemIsAddedIntoTheFavouriteList()
         {
             favourite.AddItem(id, _price);
         }
 
-        [Then("The item should be in the FavouriteBasket")]
-        public void ThenTheItemShouldBeInFavouriteBasket()
+        [Then("the favourite list should contain the item with id (.*)")]
+        public void ThenTheFavouriteListShouldContainTheItemWithId(int id)
         {
             favourite.Items.Should().Contain(x => x.CatalogItemId == id && x.UnitPrice == _price);
         }
 
 
-        [Given("A Favourite Item id (.*)")]
-        
-        public void GivenItemId(int catalogItemId)
+        [Given("a favourite list that contains an item with id (.*)")]
+        public void GivenAFavouriteListThatContainsAnItemWithId(int catalogItemId)
         {
             id = catalogItemId;
-           
         }
 
-        [When("The item is deleted from FavouriteBasket")]
+        [When("The item is deleted from favourite list")]
         public void WhenTheItemIsDeletedFromFavouriteBasket()
         {
             favourite.Delete(id);
-                }
+        }
 
-        [Then("The item should be removed from FavouriteBasket")]
+        [Then("the favourite list should not contain the item with id (.*)")]
         public void ThenTheItemShouldBeRremovedFromFavouriteBasket()
         {
             favourite.Items.Should().NotContain(x=> x.CatalogItemId == id);
         }
-
-
     }
 }
